11e01bf46209fa8b873e2b34f00f9e562502e14b
Farrukh Makhkamov <makhkamovfar@gmail.com>
2023-05-26 18:33:31 +0500

    fixed loader

--- public/main.js
+++ public/main.js
@@ -7,8 +7,8 @@ const greenCaretSVG='<svg width="30px" height="30px" viewBox="0 0 24 24" fill="n
 const redCaretSVG='<svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#f00000" transform="matrix(1, 0, 0, -1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Arrow / Caret_Up_MD"> <path id="Vector" d="M8 14L12 10L16 14" stroke="#f00000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>'
 
 getBTN.addEventListener('click', async ()=>{
-    const respone= await fetch(API_URL)
     wrapper.innerHTML='<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>'
+    const respone= await fetch(API_URL)
     const parsedResponse=await respone.json() 
     wrapper.innerHTML=''
     parsedResponse.forEach(item=>{
