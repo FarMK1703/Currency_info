85abf87fe8d6b15b18f70089faa0adc8cd6455e2
Farrukh Makhkamov <makhkamovfar@gmail.com>
2023-05-26 18:22:34 +0500

    second commit

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ const getBTN=document.getElementById('getBTN')
 const wrapper=document.querySelector('.wrapper')
 
 
-const API_URL='http://localhost:3000/getInfo'
+const API_URL='https://currency-info.onrender.com/getInfo'
 const greenCaretSVG='<svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#34f000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Arrow / Caret_Up_MD"> <path id="Vector" d="M8 14L12 10L16 14" stroke="#34f000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>'
 const redCaretSVG='<svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#f00000" transform="matrix(1, 0, 0, -1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Arrow / Caret_Up_MD"> <path id="Vector" d="M8 14L12 10L16 14" stroke="#f00000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>'
 
